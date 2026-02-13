635da404c037f925d5fcc0bdb592adf43bb0265e
sem de winter <semdewinter2@gmail.com>
2026-02-13 15:29:56 +0100

    PWA service worker

--- app.js
+++ app.js
@@ -395,3 +395,11 @@ window.addEventListener("load", () => {
 
   updatePreview();
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .catch((err) => console.error("SW registration failed:", err));
+  });
+}
